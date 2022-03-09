d0998f03a5a2afa86f9fe3575dccd698f32fc691
Rishav <73383485+Rishav5013@users.noreply.github.com>
2022-03-10 01:34:42 +0530

    Initial commit

--- ticket.js
+++ ticket.js
@@ -1,7 +1,24 @@
-const express = require ("express")
-const app = express();
-app.listen(3000,function(req,res){
-    console.log("server is runnung at port 3000 ")
+const nodemailer = require("nodemailer")
+
+const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+        user: "abc121@outlook.com",
+        password:"1234567"
+    }
+});
+
+const options = {
+    from:"abc121@outlook.com",
+    to:"defg5678@outlook.com",
+    subject: "Booking Confirmation",
+    text:"Thankyou for Booking your ticket"
+};
+
+transporter.sendMail(options, function(err, info){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Email Sent : "+ info.response);
 })
-
-
